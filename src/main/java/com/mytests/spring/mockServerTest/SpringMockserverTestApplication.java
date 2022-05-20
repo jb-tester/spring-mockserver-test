5701eac9192e80bbd14c27afc911b6c9a3363496
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-05-20 13:30:12 +0200

    Initial commit

---
+++ src/main/java/com/mytests/spring/mockServerTest/SpringMockserverTestApplication.java
@@ -0,0 +1,13 @@
+package com.mytests.spring.mockServerTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMockserverTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMockserverTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mytests/spring/mockServerTest/SpringMockserverTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytests.spring.mockServerTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockserverTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
